97b65030202f96a27ae41eb73d82a5128f632a3b
vinceh121 <contact@vinceh121.me>
2023-02-01 04:55:29 +0100

    fix: exit on uncaught exception

--- desktop/src/me/vinceh121/wanderer/desktop/DesktopLauncher.java
+++ desktop/src/me/vinceh121/wanderer/desktop/DesktopLauncher.java
@@ -13,15 +13,21 @@ public class DesktopLauncher {
 		final Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.enableGLDebugOutput(true, System.out);
 		config.setBackBufferConfig(8, 8, 8, 8, 16, 0, 4); // all default except for 4× anti aliasing
-		new Lwjgl3Application(new Wanderer(), config) {
-			@Override
-			public Lwjgl3Audio createAudio(final Lwjgl3ApplicationConfiguration config) {
-				try {
-					return new OpenAL3DAudio();
-				} catch (final OpenALException e) {
-					throw new RuntimeException(e);
+		try {
+			new Lwjgl3Application(new Wanderer(), config) {
+				@Override
+				public Lwjgl3Audio createAudio(final Lwjgl3ApplicationConfiguration config) {
+					try {
+						return new OpenAL3DAudio();
+					} catch (final OpenALException e) {
+						throw new RuntimeException(e);
+					}
 				}
-			}
-		};
+			};
+		} catch (Exception e) {
+			System.err.println("Uncaught braught back up to main!");
+			e.printStackTrace();
+			System.exit(-1);
+		}
 	}
 }
