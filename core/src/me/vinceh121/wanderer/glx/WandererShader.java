809fa1b11ac60111863520d0d8ad0e8705ea78e1
vinceh121 <contact@vinceh121.me>
2023-02-04 04:29:42 +0100

    chore: revert super.bindMaterial inline

--- core/src/me/vinceh121/wanderer/glx/WandererShader.java
+++ core/src/me/vinceh121/wanderer/glx/WandererShader.java
@@ -39,39 +39,7 @@ public class WandererShader extends DefaultShader {
 
 	@Override
 	protected void bindMaterial(final Attributes attributes) {
-		// start inline super.bindMaterial(attributes);
-		int cullFace = config.defaultCullFace == -1 ? defaultCullFace : config.defaultCullFace;
-		int depthFunc = config.defaultDepthFunc == -1 ? defaultDepthFunc : config.defaultDepthFunc;
-		float depthRangeNear = 0f;
-		float depthRangeFar = 1f;
-		boolean depthMask = true;
-
-		for (final Attribute attr : attributes) {
-			final long t = attr.type; // BAD BEHAVIOUR
-				// In the following, libgdx seems to consider t is a bitfield for the attributes array, and not a single type
-			if (BlendingAttribute.is(t)) {
-				context.setBlending(true,
-						((BlendingAttribute) attr).sourceFunction,
-						((BlendingAttribute) attr).destFunction);
-				set(u_opacity, ((BlendingAttribute) attr).opacity);
-			} else if ((t & IntAttribute.CullFace) == IntAttribute.CullFace)
-				cullFace = ((IntAttribute) attr).value;
-			else if ((t & FloatAttribute.AlphaTest) == FloatAttribute.AlphaTest)
-				set(u_alphaTest, ((FloatAttribute) attr).value);
-			else if ((t & DepthTestAttribute.Type) == DepthTestAttribute.Type) {
-				DepthTestAttribute dta = (DepthTestAttribute) attr;
-				depthFunc = dta.depthFunc;
-				depthRangeNear = dta.depthRangeNear;
-				depthRangeFar = dta.depthRangeFar;
-				depthMask = dta.depthMask;
-			} else if (!config.ignoreUnimplemented)
-				throw new GdxRuntimeException("Unknown material attribute: " + attr.toString());
-		}
-
-		context.setCullFace(cullFace);
-		context.setDepthTest(depthFunc, depthRangeNear, depthRangeFar);
-		context.setDepthMask(depthMask);
-		// end inline super.bindMaterial
+		super.bindMaterial(attributes);
 		for (final Attribute a : attributes) {
 			if (a.type == TiledMaterialAttribute.TiledMaterial) {
 				this.set(this.u_tiledMaterialOpacity, ((TiledMaterialAttribute) a).getOpacity());
