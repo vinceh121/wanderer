bd637e75d131f9863530d7c9c869458f876b8431
vinceh121 <contact@vinceh121.me>
2023-02-12 05:10:42 +0100

    feat: setInterval

--- core/src/me/vinceh121/wanderer/script/JsTimers.java
+++ core/src/me/vinceh121/wanderer/script/JsTimers.java
@@ -11,11 +11,15 @@ import org.mozilla.javascript.Undefined;
 public class JsTimers {
 	private static final JsTimers INSTANCE = new JsTimers();
 	private final Map<Integer, Timeout> timeouts = new Hashtable<>();
+	private final Map<Integer, Interval> intervals = new Hashtable<>();
 	private int nextId;
 
 	public void install(final Scriptable scope) {
 		JsUtils.install(scope, "setTimeout", this::setTimeout);
 		JsUtils.install(scope, "clearTimeout", this::clearTimeout);
+		
+		JsUtils.install(scope, "setInterval", this::setInterval);
+		JsUtils.install(scope, "clearInterval", this::clearInterval);
 	}
 
 	private Object setTimeout(final Context ctx, final Scriptable lscope, final Scriptable thisObj, final Object[] args) {
@@ -23,7 +27,7 @@ public class JsTimers {
 		final Timeout timeout;
 
 		if (args.length == 2) {
-			timeout = new Timeout(ctx, lscope, thisObj, (Callable) args[0], ((Number) args[1]).longValue());
+			timeout = new Timeout(ctx, lscope, thisObj, (Callable) args[0], ((Number) args[1]).floatValue());
 		} else if (args.length > 2) {
 			final Object[] timeoutArgs = new Object[args.length - 2];
 			System.arraycopy(args, 2, timeoutArgs, 0, args.length - 2);
@@ -31,14 +35,15 @@ public class JsTimers {
 					lscope,
 					thisObj,
 					(Callable) args[0],
-					((Number) args[1]).longValue(),
+					((Number) args[1]).floatValue(),
 					timeoutArgs);
 		} else {
 			throw new IllegalStateException("setTimeout() needs arguments");
 		}
-		this.timeouts.put(this.nextId++, timeout);
+		int id = this.nextId++;
+		this.timeouts.put(id, timeout);
 
-		return this.nextId;
+		return id;
 	}
 
 	private Object clearTimeout(final Context lcx, final Scriptable lscope, final Scriptable thisObj, final Object[] args) {
@@ -49,13 +54,54 @@ public class JsTimers {
 		return Undefined.instance;
 	}
 
+	private Object setInterval(final Context ctx, final Scriptable lscope, final Scriptable thisObj, final Object[] args) {
+		/// XXX Note: unlike spec-compliant Javascript, the this value is properly kept.
+		final Interval interval;
+
+		if (args.length == 2) {
+			interval = new Interval(ctx, lscope, thisObj, (Callable) args[0], ((Number) args[1]).floatValue());
+		} else if (args.length > 2) {
+			final Object[] intervalArgs = new Object[args.length - 2];
+			System.arraycopy(args, 2, intervalArgs, 0, args.length - 2);
+			interval = new Interval(ctx,
+					lscope,
+					thisObj,
+					(Callable) args[0],
+					((Number) args[1]).floatValue(),
+					intervalArgs);
+		} else {
+			throw new IllegalStateException("setInterval() needs arguments");
+		}
+		int id = this.nextId++;
+		this.intervals.put(id, interval);
+
+		return id;
+	}
+	
+	private Object clearInterval(final Context lcx, final Scriptable lscope, final Scriptable thisObj, final Object[] args) {
+		if (args.length != 1) {
+			throw new IllegalArgumentException("clearInterval() must have 1 argument");
+		}
+		this.intervals.remove(((Number) args[0]).intValue());
+		return Undefined.instance;
+	}
+	
 	public void update(final float delta) {
 		for (final int id : this.timeouts.keySet()) {
-			final Timeout t = this.timeouts.get(id);
-			t.timeRemaining -= (long) (delta * 1000);
-			if (t.timeRemaining < 0) {
+			final Timeout timeout = this.timeouts.get(id);
+			timeout.timeRemaining -= (delta * 1000);
+			if (timeout.timeRemaining < 0) {
 				this.timeouts.remove(id);
-				t.function.call(t.ctx, t.scope, t.thys, t.args);
+				timeout.function.call(timeout.ctx, timeout.scope, timeout.thys, timeout.args);
+			}
+		}
+		
+		for (final int id : this.intervals.keySet()) {
+			final Interval interval = this.intervals.get(id);
+			interval.timeRemaining -= (delta * 1000);
+			if (interval.timeRemaining < 0) {
+				interval.function.call(interval.ctx, interval.scope, interval.thys, interval.args);
+				interval.timeRemaining = interval.delay;
 			}
 		}
 	}
@@ -68,18 +114,42 @@ public class JsTimers {
 		return JsTimers.INSTANCE;
 	}
 
+	private static class Interval {
+		private final Context ctx;
+		private final Scriptable scope, thys;
+		private final Callable function;
+		private final Object[] args;
+		private float delay;
+		private float timeRemaining;
+
+		public Interval(final Context ctx, final Scriptable scope, final Scriptable thys, final Callable function, final float delay) {
+			this(ctx, scope, thys, function, delay, new Object[0]);
+		}
+
+		public Interval(final Context ctx, final Scriptable scope, final Scriptable thys, final Callable function, final float delay,
+				final Object[] args) {
+			this.ctx = ctx;
+			this.scope = scope;
+			this.thys = thys;
+			this.function = function;
+			this.delay = delay;
+			this.timeRemaining = delay;
+			this.args = args;
+		}
+	}
+	
 	private static class Timeout {
 		private final Context ctx;
 		private final Scriptable scope, thys;
 		private final Callable function;
 		private final Object[] args;
-		private long timeRemaining;
+		private float timeRemaining;
 
-		public Timeout(final Context ctx, final Scriptable scope, final Scriptable thys, final Callable function, final long timeRemaining) {
+		public Timeout(final Context ctx, final Scriptable scope, final Scriptable thys, final Callable function, final float timeRemaining) {
 			this(ctx, scope, thys, function, timeRemaining, new Object[0]);
 		}
 
-		public Timeout(final Context ctx, final Scriptable scope, final Scriptable thys, final Callable function, final long timeRemaining,
+		public Timeout(final Context ctx, final Scriptable scope, final Scriptable thys, final Callable function, final float timeRemaining,
 				final Object[] args) {
 			this.ctx = ctx;
 			this.scope = scope;
