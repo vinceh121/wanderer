87f82c4ac8aab7cfc88af418ba967280b94c2197
vinceh121 <contact@vinceh121.me>
2023-03-11 17:10:23 +0100

    feat: subtitle background

--- core/src/me/vinceh121/wanderer/ui/Subtitle.java
+++ core/src/me/vinceh121/wanderer/ui/Subtitle.java
@@ -1,8 +1,10 @@
 package me.vinceh121.wanderer.ui;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout.GlyphRun;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 
 import me.vinceh121.wanderer.WandererConstants;
@@ -11,7 +13,27 @@ public class Subtitle extends Label {
 	public Subtitle() {
 		super("", WandererConstants.getDevSkin());
 		this.setAlignment(Align.center);
-		this.getStyle().background = new TextureRegionDrawable(WandererConstants.BLACK_PIXEL);
+	}
+
+	@Override
+	public void draw(Batch batch, float parentAlpha) {
+		final float margin = 5;
+
+		for (GlyphLayout layout : this.getBitmapFontCache().getLayouts()) {
+			for (GlyphRun run : layout.runs) {
+				final float lineHeight = getBitmapFontCache().getFont().getLineHeight();
+				// i don't understand why this is right
+				final float y = (this.getY(Align.bottomLeft) - run.y + lineHeight * 2) - margin;
+
+				batch.draw(WandererConstants.BLACK_PIXEL,
+						run.x - margin,
+						y,
+						run.width + margin * 2,
+						lineHeight + margin * 2);
+			}
+		}
+
+		super.draw(batch, parentAlpha);
 	}
 
 	@Override
